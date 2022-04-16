d883efe38db6993717e55ec38ea6b5f71c678160
HenryTran <henrykhoatran@gmail.com>
2022-04-16 20:57:11 +1000

    yeya

---
+++ KImasu_Roster/rosters js/Sidebar.js
@@ -0,0 +1,6 @@
+function openForm() {
+    document.getElementById("numberForm").style.display="block";
+}
+function closeForm()    {
+    document.getElementById("numberForm").style.display="none";
+}
