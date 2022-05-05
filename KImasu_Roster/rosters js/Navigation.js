b3da0c34bf00f0d2a49a5886c28d3f711a007578
HenryTran <henrykhoatran@gmail.com>
2022-05-05 20:05:16 +1000

    added more stuff

--- KImasu_Roster/rosters js/Navigation.js
+++ KImasu_Roster/rosters js/Navigation.js
@@ -1,37 +1,36 @@
+/* cookie generator */
+function makeCookie(name,val,days)  {
+    const d = new Date();
+    d.setTime(d.getTime()+(days*24*60*60*1000));
+    let expire = 'expires'+d.toUTCString();
+    document.cookie = name+'='+val+';'+expire+';path=/'+d;
+}
+
+
 /* Randomly generated key*/
 function validateKey() {
-    var managers = "banana";
-    var staff = "rat";
+    var managersLogin = "Tina is cute";
+    var staffLogin = "Andrew is cute";
     const userInput = document.getElementById("login-Key");
 
-    if (userInput.value.match(managers))    {
+    if (userInput.value.match(managersLogin))    {
         window.location.href = "C:/Users/henry/Documents/Projects/KImasu_Roster/rosters html/Miki_Login.html";
-        document.cookie = "manager"
         return true;
     }
-    else if (userInput.value.match(staff))  {
+    else if (userInput.value.match(staffLogin))  {
         window.location.href = "C:/Users/henry/Documents/Projects/KImasu_Roster/rosters html/Display_Rosters.html";
-        document.cookie = "staff"
         return true;
     }
-    else{
+    else    {
         alert("Invalid key");
         return false;
     }
 }
 
+
 /* Database of staff names and numbers - should be able to be appended via web */
 var people = [];
 class Staff {
-    constructor(name,number)
-    {
-        this.name = name;
-        this.name = number;
-    }
-}
-
-export {Staff, people, validateKey}
-{
     constructor(name,number)
     {
         this.name = name;
